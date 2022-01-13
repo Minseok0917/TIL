86776b0e9de9f098bdcd8d9edac3c12edbb1b4a1
Minseok0917 <nminseok0917@naver.com>
2022-01-13 16:33:53 +0900

    vuepress default theme style 옮겨서 연결

---
+++ src/.vuepress/theme/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<Foo-Bar />
+		<div class="theme-default-content content__default">
+			<Content />
+		</div>
+	</div>
+</template>
+<script>
+	import { resolveSidebarItems } from '../utils';
+	export default{
+		mounted(){
+			console.log(resolveSidebarItems(
+				this.$page,
+				this.$page.regularPath,
+				this.$site,
+				this.$localePath
+			));
+		}
+	}
+</script>
+<style lang="stylus" src="../styles/index.styl"></style>
