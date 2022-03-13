1be79c0b01f690685a07aa6bb71b2058972d2624
Minseok0917 <nminseok0917@naver.com>
2022-03-13 23:34:56 +0900

    Board Component 추가

---
+++ src/.vuepress/components/Board.vue
@@ -0,0 +1,20 @@
+<template>
+	<article class="board-container">
+		<h2>{{title}}</h2>
+		<div class="item-container">
+			<div class="item" v-for="board in boards" :key="board.path" :data-date="board.frontmatter.creationDate">
+				<h3 class="title">{{board.frontmatter.title}}</h3>
+				<pre class="description">{{board.frontmatter.description}}</pre>
+				<a class="btn" :href="'.'+board.path">Read More</a>
+				<ul class="tags">
+					<li class="tag" v-for="tag in board.frontmatter.tags" :key="tag">#{{tag}}</li>
+				</ul>
+			</div>
+		</div>
+	</article>
+</template>
+<script>
+	export default {
+		props:['title','boards']
+	};
+</script>
